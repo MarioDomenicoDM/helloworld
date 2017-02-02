0c46b7b899079da3e18f1e540f179b64d73633bd
MarioDomenicoDM <mariodomy99@gmail.com>
2017-02-02 08:44:55 +0000

    Update SecondoGrado.py

--- SecondoGrado.py
+++ SecondoGrado.py
@@ -1,34 +1,34 @@
 import math
 
-print "programma che risolve un'equazione di secondo grado nella forma 'ax^2+bx+c=0'"
+print "Programma che risolve un'equazione di secondo grado nella forma 'ax^2+bx+c=0'"
 
-a = input("Inserisci a")
-b = input("inserisci b")
-c = input("inserisci c")
+a = input("Inserisci a ")
+b = input("inserisci b ")
+c = input("inserisci c ")
 
-if b!=0 and c!=0:
-	print "equazione completa"
-	d = (b*b)-(4*a*c)
-	x1 = (-b+math.sqrt(d))/(2*a)
-	x2 = (-b-math.sqrt(d))/(2*a)
+if a==0 and b==0 and c==0:
+    	print "L'equazione e' indeterminata"
 
+
+
+elif a==0 and b==0:
+    	print "L'equazione e' impossibile"
+
+
+
+elif a==0:
+    	x = -c/b
+    	print "La x vale", x
+
+
+else:	
+	d = (b*b)-(4.*a*c)
+	if d>=0:
+		x1 = (-b+math.sqrt(d))/(2.*a)
+		x2 = (-b-math.sqrt(d))/(2.*a)
+		print "Delta = ", d 
+		print "x1 = ", x1
+		print "x2 = ", x2
+	else:
+		print "L'equazione non ha nessuna soluzione"
 	
-	print "x1=", x1
-	print "x2=", x2
-
-elif b==0 and c!=0:
-	print "equazione pura"
-	x1 = +math.sqrt(-(c/a))
-	x2 = -math.sqrt(-(c/a))
-
-	print "x1=", x1
-        print "x2=", x2
-
-
-elif b!=0 and c==0:
-	print "equazione spuria"
-	x1 = 0
-	x2 = -(b/a)
-
-	print "x1=", x1
-        print "x2=", x2
